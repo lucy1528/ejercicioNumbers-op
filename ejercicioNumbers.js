491b295b3a0e5d6769a203fdefcd224545850fa1
lucy1528 <luxysm@gmail.com>
2022-08-25 16:31:44 -0500

    Ejercicio Numeros openBootcamp

---
+++ ejercicioNumbers.js
@@ -0,0 +1,6 @@
+let alturaCms = 167
+let alturaMtrs = alturaCms/100
+let peso = 60.500
+let redondeoAltura = Math.ceil(alturaMtrs)
+let redondeoPeso = Math.floor(peso)
+let maximoValor = Number.MAX_VALUE+1 === Number.MAX_VALUE
